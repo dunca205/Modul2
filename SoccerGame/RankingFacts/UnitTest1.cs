1b3268baadaddc6737e272a47efef776cdcd1380
Cristina Dunca <dunca205@gmail.com>
2022-09-09 11:59:07 +0300

    removed one test

--- SoccerGame/RankingFacts/UnitTest1.cs
+++ SoccerGame/RankingFacts/UnitTest1.cs
@@ -50,21 +50,6 @@ namespace Soccer
 
             Assert.True(initialTeams[2].ComparePoints(initialTeams[1])); // steaua are mai multe puncte decat dinamo dupa meci
         }
-        [Fact]
-        public void UpdateRanking_With0MorePointsAfterMatch()
-        {
-            Team[] initialTeams = new Team[3];
-            initialTeams[0] = new Team(teamName: "FCSB", teamPoints: 2); // dupa meci fcsb va avea tot 3 p
-            initialTeams[1] = new Team(teamName: "Dinamo", teamPoints: 3);// dupa meci diano va avea tot 3 p
-            initialTeams[2] = new Team(teamName: "Steaua", teamPoints: 1);
-            Ranking ranking = new Ranking();
-            ranking.Add(initialTeams[0]);
-            ranking.Add(initialTeams[1]);
-            ranking.Add(initialTeams[2]);
-            ranking.UpdateMatchScore(initialTeams[0], initialTeams[1], 0, 0); // dupa update ar trebui doar resort clasamentul initialTeams
-
-            Assert.True(initialTeams[0].ComparePoints(initialTeams[1])); // pe pozitia 1(initialTeams[0] trebuie sa fie Diamo cu 3 p, iar pozitia 2 initialTeams[1] Fcsb cu 2 p 
-        }
 
     }
 }
