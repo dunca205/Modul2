3e5969141a1fdacd74fb05572c75ae59d33439b4
Cristina Dunca <dunca205@gmail.com>
2022-09-19 14:00:24 +0300

    refactored Match from Text class

--- Json Second Part/Json Project/Text.cs
+++ Json Second Part/Json Project/Text.cs
@@ -11,9 +11,9 @@
 
         public IMatch Match(string text)
         {
-            return string.IsNullOrEmpty(text) || text.Length < prefix.Length || !text.StartsWith(prefix) ?
-                new FailedMatch(text) :
-                new SuccesMatch(text[prefix.Length..]);
+            return !string.IsNullOrEmpty(text) && text.StartsWith(prefix) ?
+                new SuccesMatch(text[prefix.Length..]) :
+                new FailedMatch(text);
         }
     }
 }
