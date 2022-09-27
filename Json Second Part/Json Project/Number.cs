73c4d7ce01efe9a5dd88afff22ac96202e59ec82
Cristina Dunca <dunca205@gmail.com>
2022-09-27 19:53:57 +0300

    Added Number Class + NumberFacts

---
+++ Json Second Part/Json Project/Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Json
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+            var sign = new Any("-+");
+            var exponentialSign = new Any("eE");
+            var integerChoice = new Choice(new OneOrMore(new Range(start: '0', end: '9')));
+            var integer = new Sequence(new OptionalPattern(new Character('-')), integerChoice);
+            var fractionalPart = new Sequence(new Any("."), new OneOrMore(integerChoice));
+            var exponentialPart = new Sequence(exponentialSign, new OptionalPattern(sign), new OneOrMore(new Range(start: '0', end: '9')));
+            pattern = new Sequence(integer, new OptionalPattern(fractionalPart), new OptionalPattern(exponentialPart));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ Json Second Part/JsonFacts/NumberFacts.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xunit;
+
+namespace Json
+{
+    public class NumberFacts
+    {
+        [Theory]
+        [InlineData("-123")]
+        [InlineData("-1")]
+        [InlineData("25")]
+        [InlineData("70")]
+        [InlineData("0")]
+        [InlineData("0.1")]
+        [InlineData("12.34")]
+        [InlineData("0.0001")]
+        [InlineData("10.00000001")]
+        [InlineData("12e3")]
+        [InlineData("12E3")]
+        [InlineData("12e+3")]
+        [InlineData("61e-9")]
+        [InlineData("12.34E3")]
+        [InlineData("0.01e-00")]
+        public void IsValidJsonNumber(string text)
+        {
+            IMatch match = new Number().Match(text);
+            Assert.Equal("", match.RemainingText());
+            Assert.True(match.Succes());
+        }
+
+        [Fact]
+        public void ExistentReminder()
+        {
+            IMatch match = new Number().Match("12E3ANA");
+            Assert.Equal("ANA", match.RemainingText());
+            Assert.True(match.Succes());
+        }
+
+        [Fact]
+        public void JsonNumberInvalid()
+        {
+            IMatch match = new Number().Match("-a");
+            Assert.Equal("-a", match.RemainingText());
+            Assert.False(match.Succes());
+        }
+        [Fact]
+        public void ValidIntegerAndInvalidFraction()
+        {
+            IMatch match = new Number().Match("-10.r");
+            Assert.Equal(".r", match.RemainingText());
+            Assert.True(match.Succes());
+        }
+        [Fact]
+        public void ValidIntegerAndInvalidExponential()
+        {
+            IMatch match = new Number().Match("-10E");
+            Assert.Equal("E", match.RemainingText());
+            Assert.True(match.Succes());
+        }
+    }
+}
