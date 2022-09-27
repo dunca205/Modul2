ca667678f9504fc98f5128ceda7388dd433cd8a9
Cristina Dunca <dunca205@gmail.com>
2022-09-27 19:56:22 +0300

    refactored Number class

--- Json Second Part/Json Project/Number.cs
+++ Json Second Part/Json Project/Number.cs
@@ -10,10 +10,10 @@ namespace Json
         {
             var sign = new Any("-+");
             var exponentialSign = new Any("eE");
-            var integerChoice = new Choice(new OneOrMore(new Range(start: '0', end: '9')));
+            var integerChoice = new OneOrMore(new Range(start: '0', end: '9'));
             var integer = new Sequence(new OptionalPattern(new Character('-')), integerChoice);
-            var fractionalPart = new Sequence(new Any("."), new OneOrMore(integerChoice));
-            var exponentialPart = new Sequence(exponentialSign, new OptionalPattern(sign), new OneOrMore(new Range(start: '0', end: '9')));
+            var fractionalPart = new Sequence(new Any("."), integerChoice);
+            var exponentialPart = new Sequence(exponentialSign, new OptionalPattern(sign), integerChoice);
             pattern = new Sequence(integer, new OptionalPattern(fractionalPart), new OptionalPattern(exponentialPart));
         }
 
