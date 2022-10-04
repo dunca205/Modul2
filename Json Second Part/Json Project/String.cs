c2fb54e3d1d990c7ed78c7b13fcfeadfaf6a3798
Cristina Dunca <dunca205@gmail.com>
2022-10-04 15:13:25 +0300

    refactored JsonString class

--- Json Second Part/Json Project/String.cs
+++ Json Second Part/Json Project/String.cs
@@ -6,15 +6,27 @@
 
         public JsonString()
         {
-            var codePoint = new Choice(new Range(start: '\u0020', end: '\u0021'), new Range(start: '\u0023', end: '\u005B'), new Range(start: '\u005D', end: char.MaxValue));
+            var hexCharacters = new Choice(
+                new Range(start: '0', end: '9'),
+                new Range(start: 'a', end: 'f'),
+                new Range(start: 'A', end: 'F'));
 
-            var hexCharacters = new Choice(new Range(start: '0', end: '9'), new Range(start: 'a', end: 'f'), new Range(start: 'A', end: 'F'));
-            var unicode = new Sequence(new Character('u'), new Sequence(hexCharacters, hexCharacters, hexCharacters, hexCharacters));
+            var unicode = new Sequence(
+                new Character('u'),
+                new Sequence(hexCharacters, hexCharacters, hexCharacters, hexCharacters));
 
             var escapeCharacters = new Any("/bfnrt\"\\/");
 
-            var escapeSequence = new Sequence(new Character('\\'), new Choice(unicode, escapeCharacters));
-            var character = new Choice(escapeSequence, codePoint);
+            var escapeSequence = new Sequence(
+                new Character('\\'),
+                new Choice(unicode, escapeCharacters));
+
+            var character = new Choice(
+                escapeSequence,
+                new Choice(
+                new Range(start: ' ', end: '!'),
+                new Range(start: '#', end: '['),
+                new Range(start: ']', end: char.MaxValue)));
 
             pattern = new Sequence(new Character('"'), new Many(character), new Character('"'));
         }
