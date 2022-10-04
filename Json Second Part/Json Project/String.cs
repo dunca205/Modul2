6027472a0808d524c12c5ff511548ea6a850ad92
Cristina Dunca <dunca205@gmail.com>
2022-10-04 11:43:14 +0300

    Added StringJson class + StringFacts

---
+++ Json Second Part/Json Project/String.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Json
+{
+    public class StringJson : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public StringJson()
+        {
+            var hexCharacters = new Any("0123456789abcdefABCDEF");
+            var escapeSequenceCharacters = new Any("/bfnrt\"\\/");
+            var unicode = new Sequence(new Character('u'), new Sequence(hexCharacters, hexCharacters, hexCharacters, hexCharacters));
+            var validChar = new Range(start: ' ', end: char.MaxValue);
+            var specialCharacter = new Sequence(new Character('\\'), new Choice(unicode, escapeSequenceCharacters));
+            pattern = new Sequence(new Character('"'), new Choice(specialCharacter, validChar), new Character('"'));
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
---
+++ Json Second Part/JsonFacts/StringFacts.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Xunit;
+
+namespace Json
+{
+    public class StringFacts
+    {
+        [Theory]
+        [InlineData("\"\\n\"")]
+        [InlineData("\"\\r\"")]
+     //   [InlineData("\"\"")]
+        [InlineData("\"\\u26Be\"")]
+        [InlineData("\"\\\"\"")]
+        [InlineData("\"\\/\"")]
+        [InlineData("\"a\"")]
+        [InlineData("\"⛅\"")]
+        
+      //  [InlineData("\"abc\"")]
+        public void IsValidJsonString(string text)
+        {
+            IMatch match = new StringJson().Match(text);
+            Assert.True(match.Succes());
+            Assert.Equal("", match.RemainingText());
+        }
+        
+        [Fact]
+        public void TextIsUnrecognizedExcapceCharacters()
+        {
+            IMatch match = new StringJson().Match("\"\\z\"");
+            Assert.False(match.Succes());
+            Assert.Equal("\"\\z\"", match.RemainingText());
+        }
+
+        [Fact]
+        public void TextEndsWithWithQuotes()
+        {
+            IMatch match = new StringJson().Match("\"\\n");
+            Assert.False(match.Succes());
+            Assert.Equal("\"\\n", match.RemainingText());
+
+        }
+
+        [Fact]
+        public void IsInvalidHexSequence()
+        {
+            IMatch match = new StringJson().Match("\"\\u26Bz\"");
+            Assert.False(match.Succes());
+            Assert.Equal("\"\\u26Bz\"", match.RemainingText());
+        }
+
+        [Fact]
+        public void IsUnfinisheddHexSequence()
+        {
+            IMatch match = new StringJson().Match("\"\\u26\"");
+            Assert.False(match.Succes());
+            Assert.Equal("\"\\u26\"", match.RemainingText());
+        }
+
+        //fail    [Fact] 
+        public void ContainsInvalidHexSequenceAndValidEscapeChar()
+        {
+            IMatch match = new StringJson().Match("\"\\u26Be \\r\"");
+            Assert.True(match.Succes());
+            Assert.Equal("", match.RemainingText());
+
+        }
+    }
+}
