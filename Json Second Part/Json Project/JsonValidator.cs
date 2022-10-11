7f8cc746f328e6a267f32199ef73c3f310330d77
Cristina Dunca <dunca205@gmail.com>
2022-10-11 09:43:47 +0300

    Refactored Main

--- Json Second Part/Json Project/JsonValidator.cs
+++ Json Second Part/Json Project/JsonValidator.cs
@@ -6,15 +6,16 @@
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("No text file was found");
+                return;
             }
-            else
+
+            foreach (var arg in args)
             {
-                foreach (var arg in args)
+                string text = System.IO.File.ReadAllText(arg);
+                IMatch match = new Value().Match(text);
+                if (match.Succes() && match.RemainingText().Equals(string.Empty))
                 {
-                    string text = System.IO.File.ReadAllText(arg);
-                    IMatch match = new Value().Match(text);
-                    Console.WriteLine(match.Succes() ? "The text file conforms to the JSON format" : "The text does not conform to the JSON format");
+                    Console.WriteLine("The text file conforms to the JSON format");
                 }
             }
         }
