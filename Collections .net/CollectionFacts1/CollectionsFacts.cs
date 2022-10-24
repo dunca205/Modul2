59f6c747bb47b1a0d7a7fed2524e6709a06779c2
Cristina Dunca <dunca205@gmail.com>
2022-10-24 17:05:51 +0300

    Added Collections + CollectionFacts

---
+++ Collections .net/CollectionFacts1/CollectionsFacts.cs
@@ -0,0 +1,99 @@
+global using Xunit;
+
+namespace Collections
+{
+    public class CollectionsFacts
+    {
+        [Fact]
+        public void AddOneLastElement()
+        {
+            var arraynumbers = new IntArray();
+            arraynumbers.Add(1);
+            Assert.True(arraynumbers.Contains(1));
+        }
+
+        [Fact]
+        public void CountArrayElements()
+        {
+            var arraynumbers = new IntArray();
+            arraynumbers.Add(1);
+            arraynumbers.Add(2);
+            arraynumbers.Add(10);
+            Assert.Equal(3, arraynumbers.Count());
+        }
+
+        [Fact]
+        public void SetElementOnCertainPositionAndCheckWhatElementIsOnCertainPosition()
+        {
+            var arraynumbers = new IntArray();
+            arraynumbers.Add(1);
+            arraynumbers.Add(2);
+            arraynumbers.SetElement(0, 20);
+            Assert.Equal(20, arraynumbers.Element(0));
+        }
+
+        [Fact]
+        public void ArrayContainsCertainElement()
+        {
+            var arraynumbers = new IntArray();
+            arraynumbers.Add(100);
+            Assert.True(arraynumbers.Contains(100));
+        }
+
+        [Fact]
+        public void ElementIsFoundAtCertainIndex()
+        {
+            var arraynumbers = new IntArray();
+            arraynumbers.Add(100);
+            arraynumbers.Add(300);
+            arraynumbers.Add(120);
+            Assert.Equal(0, arraynumbers.IndexOf(100));
+        }
+
+        [Fact]
+        public void InsertNewElementAtCertainPosition()
+        {
+            var arraynumbers = new IntArray();
+            arraynumbers.Add(100);
+            arraynumbers.Add(300);
+            arraynumbers.Add(120);
+            arraynumbers.Insert(0, 400);
+            Assert.Equal(400, arraynumbers.Element(0));
+        }
+
+        [Fact]
+        public void ClearArray()
+        {
+            var arraynumbers = new IntArray();
+            arraynumbers.Add(100);
+            arraynumbers.Add(300);
+            arraynumbers.Add(120);
+            arraynumbers.Clear();
+            Assert.Equal(0, arraynumbers.Count());
+        }
+
+        [Fact]
+        public void RemoveTheFirstAppearanceOfCertainNumber()
+        {
+            var arraynumbers = new IntArray();
+            arraynumbers.Add(100);
+            arraynumbers.Add(100);
+            arraynumbers.Add(120);
+            arraynumbers.Remove(100);
+            Assert.Equal(0, arraynumbers.Element(0));
+            Assert.Equal(100, arraynumbers.Element(1));
+        }
+
+        [Fact]
+        public void RemoveNumberAtCertainIndex()
+        {
+            var arraynumbers = new IntArray();
+            arraynumbers.Add(100);
+            arraynumbers.Add(101);
+            arraynumbers.Add(120);
+            arraynumbers.RemoveAt(1);
+            Assert.Equal(0, arraynumbers.Element(1));
+        }
+
+    }
+}
---
+++ Collections .net/Collections/Collections.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Collections
+{
+    public class IntArray
+    {
+        private int[] array;
+
+        public IntArray()
+        {
+            array = new int[0];
+        }
+        static void Main()
+        {
+
+        }
+
+        public void Add(int element)//
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[^1] = element;
+        }
+
+        public int Count()//
+        {
+            return array.Length;
+        }
+
+        public int Element(int index)//
+        {
+            return array[index];
+        }
+
+        public void SetElement(int index, int element)//
+        {
+            array[index] = element;
+        }
+
+        public bool Contains(int element)//
+        {
+            return array.Contains(element);
+        }
+
+        public int IndexOf(int element) //
+        {
+            return Array.IndexOf(array, element);
+        }
+
+        public void Insert(int index, int element) //
+        {
+            array[index] = element;
+        }
+
+        public void Clear() // aici nu stiu daca trebuie sa fie un sir cu 0 elemente sau sa se stearga doar valoarea de pe fiecare pozitie si sa ramana 0
+        {
+            Array.Resize(ref array, 0);
+        }
+
+        public void Remove(int element) 
+        {
+            array[IndexOf(element)] = 0;
+        }
+
+        public void RemoveAt(int index)
+        {
+            array[index] = 0;
+        }
+
+    }
+}
