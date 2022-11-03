a7dee332597101f5875e2ec3b0c855211808c65e
Cristina Dunca <dunca205@gmail.com>
2022-11-03 16:29:18 +0200

    Added ObjectArray and ObjectArrayEnum

---
+++ Collections .net/Collections/ObjectArray.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace Collections
+{
+    public class ObjectArray : IEnumerable
+    {
+        private readonly object[] objectArray;
+
+        public ObjectArray(object[] values)
+        {
+            this.objectArray = values;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public ObjectArrayEnum GetEnumerator()
+        {
+            return new ObjectArrayEnum(objectArray);
+        }
+    }
+}
---
+++ Collections .net/Collections/ObjectArrayEnum.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace Collections
+{
+    public class ObjectArrayEnum : IEnumerator
+    {
+        private readonly object[] objectArray;
+        int position = -1;
+
+        public ObjectArrayEnum(object[] objectArrayValues)
+        {
+            objectArray = objectArrayValues;
+        }
+
+        public object Current => objectArray[position];
+
+        public bool MoveNext()
+        {
+            if (position > objectArray.Length)
+            {
+                return false;
+            }
+
+            position++;
+            return true;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
