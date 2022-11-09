01f2ca1637fac5ae80d2aba82b76b207a04d9a49
Cristina Dunca <dunca205@gmail.com>
2022-11-09 12:35:58 +0200

    Added Facts on ObjectArrayFacts

--- Collections .net/CollectionFacts1/ObjectArrayFacts.cs
+++ Collections .net/CollectionFacts1/ObjectArrayFacts.cs
@@ -4,10 +4,34 @@ namespace Collections
     public class ObjectArrayFacts
     {
         [Fact]
-        public void AddElementsOnObjectArray()
+        public void AddNumberOnObjectArray()
         {
-            var objectAraay = new ObjectArray { 10, 20, 30, 40};
+            var objectArray = new ObjectArray { 10, 20, 30, 40};
+            Assert.True(objectArray.Contains(20));
+        }
 
+        [Fact]
+        public void AddWordAndCharacterOnObjectArray()
+        {
+            var objectArray = new ObjectArray { 10, 20, "mama" , 'c' };
+            Assert.True(objectArray.Contains("mama"));
+            Assert.True(objectArray.Contains('c'));
+        }
+
+        [Fact]
+        public void ReturnTrueIfElementIsFoundAndRemoved()
+        {
+            var objectArray = new ObjectArray { 10, 20, 30, 40 };
+            Assert.True(objectArray.Remove(10));
+            Assert.False(objectArray.Remove(100));
+        }
+
+        [Fact]
+        public void SetStringValueOnIntValue()
+        {
+            var objectArray = new ObjectArray { 10, 20, 30, 40 };
+            objectArray[0] = "mama";
+            Assert.Equal("mama", objectArray[0]);
         }
     }
 }
