31babb269097422a23154b8b7d99cdd18165f057
Cristina Dunca <dunca205@gmail.com>
2022-11-14 15:40:19 +0200

    Added List class

---
+++ Collections .net/CollectionFacts1/ListFacts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionFacts
+{
+    public class ListFacts
+    {
+        [Fact]
+        public void ListOfIntVariablesCheckValue()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            foreach(var number in list)
+            {
+                Assert.True(number > 0);
+            }
+        }
+
+        [Fact]
+        public void ListOfStringsCheckLength()
+        {
+            var list = new List<string> { "ana", "are", "apa" };
+            foreach(var word in list)
+            {
+                Assert.Equal(3, word.Length);
+            }
+        }
+
+        [Fact]
+        public void EnumerateElementsInList()
+        {
+            var list = new List<string> { "ana", "are", "apa" };
+            var enumerator = list.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                Assert.Equal(3, enumerator.Current.Length);
+            }
+        }
+    }
+}
---
+++ Collections .net/Collections/List.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    public class List<T> : IEnumerable
+    {
+        private T[] objectList;
+
+        public List()
+        {
+            const int minimumCapacity = 4;
+            objectList = new T[minimumCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public T this[int index]
+        {
+            get => this[index];
+
+            set
+            {
+                if (!IsValidIndex(index))
+                {
+                    return;
+                }
+
+                objectList[index] = value;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        public virtual void Add(T element)
+        {
+            ResizeArray();
+            objectList[Count++] = element;
+        }
+
+        public bool Contains(T element)
+        {
+            return IndexOf(element) != -1;
+        }
+
+        public int IndexOf(T element)
+        {
+            return Array.IndexOf(objectList, element, 0, Count);
+        }
+
+        public virtual void Insert(int index, T element)
+        {
+            if (!IsValidIndex(index))
+            {
+                return;
+            }
+
+            ResizeArray();
+            ShiftRight(index);
+            objectList[index] = element;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Array.Resize(ref objectList, 0);
+            Count = 0;
+        }
+
+        public bool Remove(T element)
+        {
+            int countAfterElementIsRemoved = Count - 1;
+
+            RemoveAt(IndexOf(element));
+
+            return Count == countAfterElementIsRemoved;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (!IsValidIndex(index))
+            {
+                return;
+            }
+
+            ShiftLeft(index);
+            Count--;
+        }
+
+        public void ResizeArray()
+        {
+            const int doubleTheCapacity = 2;
+            if (objectList.Length > Count)
+            {
+                return;
+            }
+
+            Array.Resize(ref objectList, Count * doubleTheCapacity);
+        }
+
+        public bool IsValidIndex(int index)
+        {
+            return index > -1 && index < Count;
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                objectList[i] = objectList[i + 1];
+            }
+        }
+
+        private void ShiftRight(int index)
+        {
+            int rightSideLimit = Count;
+            for (int i = Count; i >= Count - index; i--)
+            {
+                objectList[rightSideLimit] = objectList[i - 1];
+                rightSideLimit--;
+            }
+        }
+    }
+}
