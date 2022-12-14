a330fa52019fa49c289342378779b191a2bcdbee
Cristina Dunca <dunca205@gmail.com>
2022-12-14 13:45:35 +0200

    refactored

--- Collections .net/StreamFacts/StreamFacts.cs
+++ Collections .net/StreamFacts/StreamFacts.cs
@@ -1,17 +1,15 @@
-﻿using System.IO.Compression;
-using System.Security.Cryptography;
-using Xunit;
+﻿using Xunit;
 
 namespace StreamNamespace
 {
-    public class StreamFacts
+    public class Stream
     {
         [Fact]
         public void CompressALargeText_CompressedStreamIsSmaller()
         {
             var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
             var memorystream = new MemoryStream();
-            Write(memorystream, text, false, true);
+            StreamClass.Write(memorystream, text, false, true);
             Assert.True(memorystream.Length < text.Length);
         }
 
@@ -20,8 +18,8 @@ namespace StreamNamespace
         {
             var text = "cristina";
             var memorystream = new MemoryStream();
-            Write(memorystream, text, false, false);
-            string memoryStreamContent = Read(memorystream, false, false);
+            StreamClass.Write(memorystream, text, false, false);
+            string memoryStreamContent = StreamClass.Read(memorystream, false, false);
             Assert.Equal(text, memoryStreamContent);
         }
 
@@ -30,7 +28,7 @@ namespace StreamNamespace
         {
             var text = "cristina";
             var memorystream = new MemoryStream();
-            Write(memorystream, text, false, true);
+            StreamClass.Write(memorystream, text, false, true);
             Assert.True(memorystream.Length != text.Length);
         }
 
@@ -39,19 +37,9 @@ namespace StreamNamespace
         {
             var text = "cristina";
             var memorystream = new MemoryStream();
-            Write(memorystream, text, false, true);
-            int decompressedTextLength = Read(memorystream, false, true).Length;
+            StreamClass.Write(memorystream, text, false, true);
+            int decompressedTextLength = StreamClass.Read(memorystream, false, true).Length;
             Assert.Equal(text.Length, decompressedTextLength);
-        }
-
-        [Fact]
-        public void EncryptTextAndRead_EncryptedTextIsDifferentThanText()
-        {
-            var text = "cristina";
-            var memorystream = new MemoryStream();
-            Write(memorystream, text, true, false);
-            string cryptedText = (Read(memorystream, false, false));
-            Assert.NotEqual(cryptedText, text);
         }
 
         [Fact]
@@ -59,8 +47,8 @@ namespace StreamNamespace
         {
             var text = "cristina";
             var memorystream = new MemoryStream();
-            Write(memorystream, text, true, false);
-            string decryptedText = Read(memorystream, true, false);
+            StreamClass.Write(memorystream, text, true, false);
+            string decryptedText = StreamClass.Read(memorystream, true, false);
             Assert.Equal(text, decryptedText);
         }
 
@@ -72,47 +60,6 @@ namespace StreamNamespace
             StreamClass.Write(memorystream, text, true, true);
             string decryptedAndDecompressedText = StreamClass.Read(memorystream, true, true);
             Assert.Equal(text, decryptedAndDecompressedText);
-        }
-
-        static Aes aesAlgorithm = Aes.Create();
-        static string Read(Stream stream, bool decrypt = false, bool decompress = false)
-        {
-            stream.Seek(0, SeekOrigin.Begin);
-
-            if (decompress)
-            {
-                stream = new GZipStream(stream, CompressionMode.Decompress);
-            }
-
-            if (decrypt)
-            {
-                stream = new CryptoStream(stream, aesAlgorithm.CreateDecryptor(), CryptoStreamMode.Read);
-            }
-
-            StreamReader fromStream = new StreamReader(stream);
-            return fromStream.ReadToEnd(); ;
-        }
-
-        static void Write(Stream stream, string text, bool encrypt = false, bool compress = false)
-        {
-            if (compress)
-            {
-                stream = new GZipStream(stream, CompressionMode.Compress);
-            }
-
-            if (encrypt)
-            {
-                stream = new CryptoStream(stream, aesAlgorithm.CreateEncryptor(), CryptoStreamMode.Write);
-            }
-
-            StreamWriter toStream = new StreamWriter(stream);
-            toStream.Write(text);
-            toStream.Flush();
-
-            if (stream is CryptoStream encrypted)
-            {
-                encrypted.FlushFinalBlock();
-            }
         }
     }
 }
