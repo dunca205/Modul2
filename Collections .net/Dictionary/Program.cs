bc657ffb2999a27038e610dae6c606ab39064b65
Cristina Dunca <dunca205@gmail.com>
2023-01-17 18:35:20 +0200

    added Dictionary

---
+++ Collections .net/Dictionary/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Dictionary
+{
+    public class Dictionaryy<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        
+        public Dictionaryy() 
+        { }
+
+        public TValue this[TKey key] { get; set; }
+
+        public ICollection<TKey> Keys { get; }
+        public ICollection<TValue> Values { get; }
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+
+        public void Add(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
