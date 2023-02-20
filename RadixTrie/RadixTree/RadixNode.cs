f69cd0b3b5756fb397120cccbc3d12fd6ecd7605
Cristina Dunca <dunca205@gmail.com>
2023-02-20 13:43:09 +0200

    Insertion works well

--- RadixTrie/RadixTree/RadixNode.cs
+++ RadixTrie/RadixTree/RadixNode.cs
@@ -10,102 +10,10 @@
             children = new SortedList<char, RadixNode<string>>();
         }
 
-        public SortedList<char, RadixNode<string>> Children { get => this.children; set => this.children = value; }
+        public SortedList<char, RadixNode<string>> Children { get => this.children; }
 
         public string Value { get; set; }
 
         public bool IsWord { get; set; }
-
-        public void AddChild(string stringValue, int index)
-        {
-            var key = children.GetKeyAtIndex(index);
-            Matching(ref stringValue, children[key]);
-
-            if (stringValue.Length == 0)
-            {
-                return;
-            }
-
-            SplitNode(children[key], stringValue, out RadixNode<string> temp);
-            children[key] = temp;
-        }
-
-        private static string GetSubstring(string stringValue, string valueOfRadix)
-        {
-            return stringValue[valueOfRadix.Length..];
-        }
-
-        private static void Next(ref RadixNode<string> existingNode, ref string leftOver, ref char key)
-        {
-            if (leftOver.StartsWith(existingNode.Value))
-            {
-                leftOver = GetSubstring(leftOver, existingNode.Value);
-
-                if (existingNode.children.IndexOfKey(leftOver[0]) > -1)
-                {
-                    key = leftOver[0];
-                    return;
-                }
-
-                existingNode.children.Add(leftOver[0], new RadixNode<string>(leftOver));
-                leftOver = "";
-                key = ' ';
-                return;
-            }
-
-            SplitNode(existingNode, leftOver, out RadixNode<string> temp);
-            existingNode = temp;
-            key = ' ';
-            leftOver = "";
-        }
-
-        private static void SplitNode(RadixNode<string> existingNode, string value, out RadixNode<string> temp)
-        {
-            string existingNodeValue = existingNode.Value;
-            string commonRadix = "";
-            for (int i = 0; i < existingNodeValue.Length && i < value.Length && existingNodeValue[i] == value[i]; i++)
-            {
-                commonRadix += existingNodeValue[i];
-            }
-
-            existingNodeValue = GetSubstring(existingNodeValue, commonRadix);
-            value = GetSubstring(value, commonRadix);
-
-            temp = new RadixNode<string>(commonRadix);
-            temp.children.Add(value[0], new RadixNode<string>(value));
-            temp.children[value[0]].IsWord = true;
-            existingNode.Value = existingNodeValue;
-            temp.children.Add(existingNodeValue[0], existingNode);
-        }
-
-        private static void Matching(ref string newValue, RadixNode<string> existingNode)
-        {
-            string existingNodeValue = existingNode.Value;
-
-            if (!newValue.StartsWith(existingNodeValue))
-            {
-                return;
-            }
-
-            newValue = GetSubstring(newValue, existingNodeValue);
-
-            int indexOfNextKey = existingNode.children.IndexOfKey(newValue[0]);
-            if (indexOfNextKey != -1)
-            {
-                var key = newValue[0];
-
-                while (key != ' ')
-                {
-                    existingNode = existingNode.children[key];
-                    Next(ref existingNode, ref newValue, ref key);
-                }
-
-                return;
-            }
-
-            existingNode.children.Add(newValue[0], new RadixNode<string>(newValue));
-            existingNode.children[newValue[0]].IsWord = true;
-            newValue = "";
-        }
     }
 }
--- RadixTrie/RadixTree/RadixTree.cs
+++ RadixTrie/RadixTree/RadixTree.cs
@@ -12,15 +12,74 @@
         public void Insert(T value)
         {
             string stringValue = value.ToString();
-            int indexOfnewValue = root.Children.IndexOfKey(stringValue[0]);
-            if (indexOfnewValue != -1)
+            char key = stringValue[0];
+            if (root.Children.ContainsKey(stringValue[0]))
             {
-                root.AddChild(stringValue, indexOfnewValue);
+                Add(ref stringValue, ref key);
                 return;
             }
 
-            root.Children.Add(stringValue[0], new RadixNode<string>(stringValue));
-            root.Children[stringValue[0]].IsWord = true;
+            root.Children.Add(key, new RadixNode<string>(stringValue));
+        }
+
+        private static void SplitNode(RadixNode<string> existingNode, string value, out RadixNode<string> newNode)
+        {
+            string existingNodeValue = existingNode.Value;
+            string commonRadix = "";
+            for (int i = 0; i < existingNodeValue.Length && i < value.Length && existingNodeValue[i] == value[i]; i++)
+            {
+                commonRadix += existingNodeValue[i];
+            }
+
+            existingNodeValue = GetSubstring(existingNodeValue, commonRadix);
+            value = GetSubstring(value, commonRadix);
+            newNode = new RadixNode<string>(commonRadix);
+            newNode.Children.Add(value[0], new RadixNode<string>(value));
+            newNode.Children[value[0]].IsWord = true;
+            existingNode.Value = existingNodeValue;
+            newNode.Children.Add(existingNodeValue[0], existingNode);
+        }
+
+        private static string GetSubstring(string stringValue, string valueOfRadix)
+        {
+            return stringValue[valueOfRadix.Length..];
+        }
+
+        private void Add(ref string value, ref char key)
+        {
+            if (value.StartsWith(root.Children[key].Value))
+            {
+                for (RadixNode<string> temp = root.Children[key]; temp.Value != null && value.Length > 0; temp = temp.Children[key])
+                {
+                    value = GetSubstring(value, temp.Value);
+                    key = value[0];
+
+                    if (temp.Children.Count == 0 || !temp.Children.ContainsKey(key)) // daca nodul nu mai are copii sau copii nu au cheia respectiva 
+                    {
+                        temp.Children.Add(key, new RadixNode<string>(value));
+                        return;
+                    }
+
+                    if (value.StartsWith(temp.Children[key].Value)) // prefix comun
+                    {
+                        key = value[0];
+                    }
+                    else
+                    {
+                        RadixNode<string> newNode;
+                        SplitNode(temp.Children[key], value, out newNode);
+                        temp.Children[key] = newNode;
+                        value = "";
+                        return;
+                    }
+                }
+
+                return;
+            }
+
+            RadixNode<string> newValue;
+            SplitNode(root.Children[key], value, out newValue);
+            root.Children[key] = newValue;
         }
     }
 }
